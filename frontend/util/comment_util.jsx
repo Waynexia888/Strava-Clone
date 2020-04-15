67350776e3b614d96b4a63e89f6b6e951056c6e0
Waynexia888 <qiwuwaynexia@gmail.com>
2020-04-15 15:50:14 -0700

    added comment utils

---
+++ frontend/util/comment_util.jsx
@@ -0,0 +1,22 @@
+export const fetchActivityComments = (activityId) => (
+    $.ajax({
+        url: `api/activities/${activityId}/comments`,
+        method: 'GET'
+    })
+
+)
+
+export const createComment = (comment) => (
+    $.ajax({
+        url: `api/comments`,
+        method: "POST",
+        data: { comment }
+    })
+)
+
+export const deleteComment = (commentId) => (
+    $.ajax({
+        url: `api/comments/${commentId}`,
+        method: 'DELETE',
+    })
+)
